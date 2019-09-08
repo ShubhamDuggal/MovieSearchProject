27f9e7fadfb5a46e2ca4f7e08a29d6e0a61c5e76
Adam McKnight <adammcknight1994@gmail.com>
2019-09-07 19:17:39 -0500

    Removed one of two cases from MovieScripts.js because the partial search case achieves the same thing as the other case. Also changed the order in which the elements are listed in each card.
    Changed styles.css, changed the format of the search box, added icon
    from font awesome to replace the search button, changed the shape and
    size of the each card.
    
    Issues that are yet to be resolved:
    
    	Enable a scroll bar that will change in size as the user
    	continues searching and more results are returned OR make the
    	page refresh each time the search button is pressed and then
    	display the card so that the results won't overflow off the
    	page.
    
    	Add the Utelly API to MovieScripts.js and add elements to the
    	card to display the sources that each title can be streamed at.

--- MovieScripts.js
+++ MovieScripts.js
@@ -1,60 +1,80 @@
+function key_down(e) {
+	if(e.keyCode == 13) {
+		search();
+	}
+}
+
 function search(){
 	const app = document.getElementById('root')
 	const logo = document.createElement('img')
-	const container = document.createElement('div')
-	
+	const container = document.getElementById('div')
 	const inputTitle = document.getElementById('Name')
-	
 	const openAddress = 'http://omdbapi.com/?t=' + inputTitle.value + '&apikey=a5834c47'
+	
+	if ($(container).children().length > 1){
+		$("div").remove();0
+	}
 	container.setAttribute('class', 'container')
 
 	var request = new XMLHttpRequest()
 	request.open('GET', openAddress, true)
-	request.onload = function() {
-	  // Begin accessing JSON data here
-	  var data = JSON.parse(this.response) //Need to improve search functionality, need to switch to other APIs, need to style
-	  if (request.status >= 200 && request.status < 400) {
-		
-			if(data.Title.toLowerCase == document.getElementById('Name').value.toLowerCase){
-			  const card = document.createElement('div')
-			  card.setAttribute('class', 'card')
 
-			  const h1 = document.createElement('h1')
-			  h1.textContent = data.Title
-
-			 const p = document.createElement('p')
-			 p.textContent = `${data.Plot} Directed by ${data.Director}. Cast: ${data.Actors}. Released ${data.Year}, Rated ${data.Rated}, Reviews: ${data.Ratings[1].Source} : ${data.Ratings[1].Value}`
-
-			  container.appendChild(card)
-			  card.appendChild(h1)
-			  card.appendChild(p)
-			  app.appendChild(container)
+	request.onload = function() { 
+		var data = JSON.parse(this.response)
+		var search = data.Title.toString()
+		if (request.status >= 200 && request.status < 400) {
+			
+			if (document.getElementById('Name').value == "") {
+				alert("Please enter a title or partial title.")
 			}
-			else {
-				if (data.Title.toLowerCase.includes(document.getElementById('Name').value.toLowerCase)){
-			  const card = document.createElement('div')
-			  card.setAttribute('class', 'card')
-
-			  const h1 = document.createElement('h1')
-			  h1.textContent = data.Title
-
-			  const p = document.createElement('p')
-			  
-			 p.textContent = `${data.Plot} Directed by ${data.Director}. Cast: ${data.Actors}. Released ${data.Year}, Rated ${data.Rated}, Reviews: ${data.Ratings[1].Source} : ${data.Ratings[1].Value}`
-			  
-			  container.appendChild(card)
-			  card.appendChild(h1)
-			  card.appendChild(p)
-			  app.appendChild(container)
-				}
+			
+			const card = document.createElement('div2')
+			card.setAttribute('class', 'card')
+			
+			const h1	   = document.createElement('h1')
+			const p        = document.createElement('p') 
+			const cast     = document.createElement('cast')
+			const rel      = document.createElement('rel')
+			const rate     = document.createElement('rate')
+			const director = document.createElement('director')
+			const reviews  = document.createElement('reviews') 
+			
+			if (data.Ratings[1] != undefined){
+				reviews.textContent = `${data.Ratings[1].Source} : ${data.Ratings[1].Value}`
 			}
-		
-	  } 
-	  else {
+			
+			h1.textContent       = data.Title
+			p.textContent 		 = `${data.Plot}`
+			cast.textContent     = `Cast: ${data.Actors}`
+			rel.textContent      = `Released: ${data.Year}`
+			rate.textContent     = `Rated: ${data.Rated}`
+			director.textContent = `Directed by ${data.Director}`
+			
+			container.appendChild(card)
+			card.appendChild(h1)
+			card.appendChild(p)
+			card.appendChild(rel)
+			card.appendChild(director)
+			card.appendChild(rate)
+			if (reviews != null){
+				card.appendChild(reviews)
+			}
+			card.appendChild(cast)
+
+			app.appendChild(container)
+	}
+		else {
 			const errorMessage = document.createElement('marquee')
-			errorMessage.textContent = `Gah, it's not working!`
+			errorMessage.textContent = `Request Failed`
 			app.appendChild(errorMessage)
 		}
 	}
 	request.send()
 }
+
+// function waitNext(e){
+	// while(e.keyCode != 13){
+		
+	// }
+	// window.location.reload();
+// }
