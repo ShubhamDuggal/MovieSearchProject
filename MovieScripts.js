4088248fb29c6dc720d83290fc5b53f9478655b9
Adam McKnight <adammcknight1994@gmail.com>
2019-09-09 10:46:15 -0500

    Added Utelly API, created new DOM element "sources" that displays sources where the title is available for streaming. Attached sources to the card.
    Changed padding around the other DOM objects inside the card for better
    presentation.
    
    Issues still to be resolved:
    
    	Add a scroll bar that grows dynamically with the page as more
    	results are returned.
    
    	Don't add sources to the card if the title is not available for
    	streaming, replace with message that lets the user know it is
    	not available for streaming
    
    	Add information to the page that the API does not work with all
    	streaming platforms (for example: Hulu and HBO Now are a few
    	platforms that Utelly does not access)

--- MovieScripts.js
+++ MovieScripts.js
@@ -1,4 +1,4 @@
-function key_down(e) {
+function key_down(e) { //allow users to press enter instead of clicking the submit button on the form
 	if(e.keyCode == 13) {
 		search();
 	}
@@ -8,28 +8,56 @@ function search(){
 	const app = document.getElementById('root')
 	const logo = document.createElement('img')
 	const container = document.getElementById('div')
+	const card = document.createElement('div')
 	const inputTitle = document.getElementById('Name')
 	const openAddress = 'http://omdbapi.com/?t=' + inputTitle.value + '&apikey=a5834c47'
 	
-	if ($(container).children().length > 1){
-		$("div").remove();0
-	}
 	container.setAttribute('class', 'container')
-
+	card.setAttribute('class', 'card')
+	
 	var request = new XMLHttpRequest()
-	request.open('GET', openAddress, true)
+	request.open('GET', openAddress, true) //Open connection to OMDb API
 
 	request.onload = function() { 
-		var data = JSON.parse(this.response)
-		var search = data.Title.toString()
+		var data = JSON.parse(this.response) //parse OMDb response
 		if (request.status >= 200 && request.status < 400) {
 			
-			if (document.getElementById('Name').value == "") {
+			if (document.getElementById('Name').value == "") { //User cannot submit empty search, alert them if they attempt this
 				alert("Please enter a title or partial title.")
 			}
 			
-			const card = document.createElement('div2')
-			card.setAttribute('class', 'card')
+			const sources = document.createElement('sources')
+			var tempSources = '';
+
+			var settings = {
+				"async": true,
+				"crossDomain": true,
+				"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=" + data.Title,
+				"method": "GET",
+				"headers": {
+					"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+					"x-rapidapi-key": "d58ce49f7bmsh6c2652635ed2fecp1ef5c6jsnf8a990d34b0b"
+				}
+			}
+
+			$.ajax(settings).done(function (response) {
+				if (response.results.length != 0){
+					for(i = 0; i < response.results.length; i++){
+						if (response.results[i].name == data.Title){
+							for (j = 0; j < response.results[i].locations.length; j++){
+								
+								if (j == response.results[i].locations.length - 1){
+									tempSources += response.results[i].locations[j].display_name
+								}
+								else{
+									tempSources += response.results[i].locations[j].display_name + ", ";
+								}
+							}
+							sources.textContent = 'Available for streaming on: ' + tempSources;
+						}
+					}
+				}
+			});
 			
 			const h1	   = document.createElement('h1')
 			const p        = document.createElement('p') 
@@ -39,8 +67,10 @@ function search(){
 			const director = document.createElement('director')
 			const reviews  = document.createElement('reviews') 
 			
-			if (data.Ratings[1] != undefined){
-				reviews.textContent = `${data.Ratings[1].Source} : ${data.Ratings[1].Value}`
+			if (data.ratings != undefined){
+				if (data.Ratings.length > 0){
+					reviews.textContent = `${data.Ratings[1].Source} : ${data.Ratings[1].Value}`
+				}
 			}
 			
 			h1.textContent       = data.Title
@@ -60,7 +90,7 @@ function search(){
 				card.appendChild(reviews)
 			}
 			card.appendChild(cast)
-
+			card.appendChild(sources)
 			app.appendChild(container)
 	}
 		else {
@@ -72,9 +102,24 @@ function search(){
 	request.send()
 }
 
-// function waitNext(e){
-	// while(e.keyCode != 13){
-		
-	// }
-	// window.location.reload();
+// function getSources{
+	// var unirest = require("unirest");
+
+	// var req = unirest("GET", "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup");
+
+	// req.query({
+		// "term": title,
+	// });
+
+	// req.headers({
+		// "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+		// "x-rapidapi-key": "d58ce49f7bmsh6c2652635ed2fecp1ef5c6jsnf8a990d34b0b"
+	// });
+
+
+	// req.end(function (res) {
+		// if (res.error) throw new Error(res.error);
+
+		// console.log(res.body);
+	// });
 // }
